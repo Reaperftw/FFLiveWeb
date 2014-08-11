4dbad7feb015e7a5ceb06cbeb621f722888cbafd
Matt Croydon <mattcroydon8@gmail.com>
2014-08-11 17:53:22 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,7 @@
+﻿var http = require('http');
+var ipaddr = '192.168.2.18';
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.end('Hello World\n');
+}).listen(1337, ipaddr);
+console.log('Server running at ' + ipaddr);
